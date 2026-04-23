b8cf32527c6e43db963a707373ee6069c90d8604
unknown <tamima.web5202@gmail.com>
2026-04-23 10:59:31 +0600

    fix: production build file replacements and auth service api url

--- admin-panel/src/app/core/services/auth.service.ts
+++ admin-panel/src/app/core/services/auth.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private apiUrl = `${environment.apiUrl}/auth`;
   private currentUserSubject = new BehaviorSubject<any>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
